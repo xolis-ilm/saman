105dfabf0804db5d7b09706c1d989d44fb4cdbf0
xolisilm <106427820+SherJS@users.noreply.github.com>
2022-10-13 19:55:50 +0500

    fist commit

---
+++ main.js
@@ -0,0 +1,35 @@
+<div className="swiper-container slider1">
+    <div className="swiper-wrapper">
+        <div className="swiper-slide"><h1>Slide 1</h1></div>
+        <div className="swiper-slide"><h1>Slide 2</h1></div>
+        <div className="swiper-slide"><h1>Slide 3</h1></div>
+    </div>
+    <div className="swiper-pagination"></div>
+    <div className="swiper-button-prev"></div>
+    <div className="swiper-button-next"></div>
+    <div className="swiper-scrollbar"></div>
+</div>
+
+<div className="swiper-container slider2">
+    <div className="swiper-wrapper">
+        <div className="swiper-slide"><h1>Slide 1</h1></div>
+        <div className="swiper-slide"><h1>Slide 2</h1></div>
+        <div className="swiper-slide"><h1>Slide 3</h1></div>
+    </div>
+    <div className="swiper-pagination"></div>
+    <div className="swiper-button-prev"></div>
+    <div className="swiper-button-next"></div>
+    <div className="swiper-scrollbar"></div>
+</div>
+
+<div className="swiper-container slider3">
+    <div className="swiper-wrapper">
+        <div className="swiper-slide"><h1>Slide 1</h1></div>
+        <div className="swiper-slide"><h1>Slide 2</h1></div>
+        <div className="swiper-slide"><h1>Slide 3</h1></div>
+    </div>
+    <div className="swiper-pagination"></div>
+    <div className="swiper-button-prev"></div>
+    <div className="swiper-button-next"></div>
+    <div className="swiper-scrollbar"></div>
+</div>
